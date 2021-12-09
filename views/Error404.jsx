bf6bfd1796c3669463a3cb0053b021b4f6d59dce
AlbertPoliarco <nikpoliarco@outlook.com>
2021-12-08 19:09:35 -0600

    Adding Bootstrap grid to index page

--- views/Error404.jsx
+++ views/Error404.jsx
@@ -7,6 +7,13 @@ function Error404 () {
           <main>
               <h1>404: PAGE NOT FOUND</h1>
               <p>Oops, sorry, we can't find this page!</p>
+              <div>
+                    <img src="/images/beach.jpg" alt="beach" />
+                </div>
+              <div>
+              Photo by <a href="https://unsplash.com/@itstarynnnn?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Taryn Kaahanui</a> on <a href="https://unsplash.com/s/photos/free?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
+              </div>
           </main>
       </Def>
     )
--- views/Home.jsx
+++ views/Home.jsx
@@ -6,6 +6,13 @@ function Home () {
         <Def>
             <main>
                 <h1>HOME</h1>
+                <div>
+                    <img src="/images/christmas.jpg" alt="christmas tree" class="center" />
+                </div>
+                <div>
+                Photo by <a href="https://unsplash.com/@chadmadden?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Chad Madden</a> on <a href="https://unsplash.com/s/photos/christmas?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
+                </div>
                 <a href="/places"><button className="btn-primary">Places Page</button>
                 </a>
             </main>
