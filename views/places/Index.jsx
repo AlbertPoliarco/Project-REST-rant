1c6af5d71eac089d44dcd67a7086d304559b7fc6
AlbertPoliarco <nikpoliarco@outlook.com>
2021-12-17 18:31:54 -0600

    updated-index-and-show-page

--- views/places/Index.jsx
+++ views/places/Index.jsx
@@ -2,18 +2,15 @@ const React = require('react')
 const Def = require('../default')
 
 function index(data) {
-    let placesFormatted = data.places.map((place, index) => {
-        return (
-          <div className="col-sm-6">
-            <a href={`/places/${index}`}>{place.name}</a>
-            <p clasName="text-center">
-              {place.cuisines}
-            </p>
-            <img src={place.pic} alt={place.name} />
-            <p clasName="text-center">
-              Located in {place.city}, {place.state}
-            </p>
-          </div>
+    let placesFormatted = data.places.map((place) => {
+      return (
+        <div className="col-sm-6">
+          <h2>
+            <a href={`/places/${place.id}`}>
+              {place.name}
+            </a>
+          </h2> 
+        </div>
         )
       })
     return (
