f9ee6b7292ccece33dd3bae6b6076a344f08ab24
AlbertPoliarco <nikpoliarco@outlook.com>
2021-12-20 18:01:02 -0600

    fixed-index-page-to-show-pictures

--- views/places/Index.jsx
+++ views/places/Index.jsx
@@ -1,22 +1,26 @@
-const React = require('react')
-const Def = require('../default')
 
-function index(data) {
-    let placesFormatted = data.places.map((place) => {
-      return (
-        <div className="col-sm-6">
-          <h2>
-            <a href={`/places/${place.id}`}>
-              {place.name}
-            </a>
-          </h2> 
-        </div>
+const React = require("react")
+const Def = require("../Default")
+
+function Index(data) {
+    let placesFormatted = data.places.map((place, index) => {
+        return (
+            <div className="col-sm-6" key={index}>
+                <h2>
+                    <a href={`/places/${place.id}`}>
+                        {place.name}
+                    </a>
+                </h2>
+                <p className="text-center">{place.cuisines}</p>
+                <img src={place.pic} alt={place.name}/>
+                <p className="text-center">Located in {place.city}, {place.state}</p>
+            </div>
         )
-      })
+    })
     return (
         <Def>
             <main>
-                <h1>RAVE ABOUT</h1>
+                <h1>Places to Rant or Rave About</h1>
                 <div className="row">
                     {placesFormatted}
                 </div>
@@ -24,4 +28,5 @@ function index(data) {
         </Def>
     )
 }
-module.exports = index
+
+module.exports = Index
